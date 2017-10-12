3c02429eacc946204124ddbf9f81b79e449886e3
Jiyoung Oh <jo289@cornell.edu>
2017-10-11 20:06:06 -0400

    fixed image

--- js/work.js
+++ js/work.js
@@ -1,11 +1,11 @@
 
 var projects = [
   //{title: "Neighborhood Map", href_path:"" , img: "", type:"Coming Soon!", description: "Google Map API, Knockout.js"},
-  //{title: "Information Science Student Association", href_path:"http://issa.infosci.cornell.edu/" , img: "", type:"Organization", description: "Web/Graphics Chair"},
+  //{title: "Information Science Student Association", href_path:"http://issa.infosci.cornell.edu/" , img: "img/issa.png", type:"Organization", description: "Web/Graphics Chair"},
   {title: "Aizen Co.", href_path:"http://www.aizen.co", img: "img/aizen.png", type:"Internship", description: "Product Mangement / Research & Analysis"},
   {title: "Pony Up", href_path:"" , img: "img/ponyup.png", type:"Game Design Class Project", description: "Product Mangement / Research & Analysis"},
   {title: "Movie Catalogue", href_path:"https://github.com/summerohoh/Movie-Trailers" , img: "img/moviecatalogue.png", type:"Internship", description: "Product Mangement / Research & Analysis"},
-  {title: "Bon VR", href_path:"" , img: "img/aizen.png", type:"Internship", description: "Product Mangement / Research & Analysis"},
+  {title: "Bon VR", href_path:"" , img: "img/bonvr.png", type:"Internship", description: "Product Mangement / Research & Analysis"},
   {title: "Taggle Inc.", href_path:"http://www.taggle.co", img: "img/taggle.png", type:"Internship", description: "Marketing / Wireframing / Data Analysis"},
   {title: "Pro Bonobos", href_path:"http://probonobos.github.io/rhok/", img: "img/probonobos.png", type:"Hackathon", description: "Web Development / Infographics"}
   ];
